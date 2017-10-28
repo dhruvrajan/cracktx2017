1afba14e9cd26981602e0ff30c09c3eb658ee278
Dhruv Rajan <dhruv@krishnaprem.com>
2017-10-28 17:51:59 -0500

    pun generation

--- src/pun_grabber.py
+++ src/pun_grabber.py
@@ -6,10 +6,11 @@
 
 import xml.etree.ElementTree as ET
 import pickle
+import random
 from collections import defaultdict
 
 
-def get_puns(pun_files):
+def extract_puns(pun_files):
     puns = defaultdict(lambda: None)
 
     for pun_file in pun_files:
@@ -27,38 +28,58 @@ def get_puns(pun_files):
 
 
 def create_inverted_index(puns, mapping_files):
-    invertedIndex = defaultdict(lambda: None)
+    inverted_index = defaultdict(lambda: None)
     for mapping_file in mapping_files:
         with open(mapping_file, 'r') as f:
             for line in f:
                 punID, word = tuple(line.strip().split())
                 wordID = int(word.split("_")[-1]) - 1
                 the_word = puns[punID][wordID]
-                if not invertedIndex[the_word]:
+                if not inverted_index[the_word]:
                     punIDList = []
                     punIDList.append(punID)
-                    invertedIndex[the_word] = punIDList
+                    inverted_index[the_word] = punIDList
                 else:
-                    invertedIndex[the_word].append(punID)
+                    inverted_index[the_word].append(punID)
 
-    return dict(invertedIndex)
+    return dict(inverted_index)
+
+
+def load_data():
+    with open("inverted_index.idx", "rb") as f:
+        inverted_index, puns = pickle.load(f)
+
+    return inverted_index, puns
+
+
+def generate_pun(phrase):
+    inverted_index, puns = load_data()
+    for word in phrase:
+        try:
+            pun_ids = inverted_index[word]
+            return puns[random.choice(pun_ids)]
+        except:
+            pass
+
+    return None
 
 
 def main():
-    puns = get_puns(['../semeval2017_task7/data/test/subtask2-heterographic-test.xml',
+    puns = extract_puns(['../semeval2017_task7/data/test/subtask2-heterographic-test.xml',
                      '../semeval2017_task7/data/test/subtask2-homographic-test.xml'])
-    invertedIndex = create_inverted_index(puns, ['../semeval2017_task7/data/test/subtask2-heterographic-test.gold',
+    inverted_index = create_inverted_index(puns, ['../semeval2017_task7/data/test/subtask2-heterographic-test.gold',
                                                  '../semeval2017_task7/data/test/subtask2-homographic-test.gold'])
 
     with open("inverted_index.idx", "wb") as f:
-        pickle.dump((invertedIndex, dict(puns)), f)
+        pickle.dump((inverted_index, dict(puns)), f)
 
     with open("inverted_index.idx", "rb") as f:
-        invertedIndex, puns = pickle.load(f)
+        inverted_index, puns = pickle.load(f)
 
-    print(invertedIndex["alleged"])
-    print(puns[invertedIndex["alleged"][0]])
-    print(invertedIndex["vault"])
+    print(inverted_index["alleged"])
+    print(puns[inverted_index["alleged"][0]])
+    print(inverted_index["vault"])
+    print(generate_pun(["bovine"]))
 
 
 if __name__ == "__main__":
